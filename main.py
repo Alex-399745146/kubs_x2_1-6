07deed85d20d2abd18a678a3b7ceacea4da395f0
alex_bachevskiy <bachevskiyaa@bk.ru>
2023-02-12 16:06:00 +0300

    add file

--- main.py
+++ main.py
@@ -1,1 +1,87 @@
-print(1)
+import random
+counter = 0
+a = random.randint(1, 6)
+b = random.randint(1, 6)
+print()
+print()
+if a == 1:
+    print(' _______')
+    print('|       |')
+    print('|   *   |    ', a)
+    print('|       |')
+    print(' -------')
+elif a == 2:
+    print(' _______')
+    print('| *     |')
+    print('|       |    ', a)
+    print('|     * |')
+    print(' -------')
+elif a == 3:
+    print(' _______')
+    print('| *     |')
+    print('|   *   |    ', a)
+    print('|     * |')
+    print(' -------')
+elif a == 4:
+    print(' _______')
+    print('| *   * |')
+    print('|       |    ', a)
+    print('| *   * |')
+    print(' -------')
+elif a == 5:
+    print(' _______')
+    print('| *   * |')
+    print('|   *   |    ', a)
+    print('| *   * |')
+    print(' -------')
+else:
+    print(' _______')
+    print('| *   * |')
+    print('| *   * |    ', a)
+    print('| *   * |')
+    print(' -------')
+if a == b:
+    print('========= КУШ:', a, a, a, a)
+    counter = a * 4
+else:
+    print('=========')
+if b == 1:
+    print(' _______')
+    print('|       |')
+    print('|   *   |    ', b)
+    print('|       |')
+    print(' -------')
+elif b == 2:
+    print(' _______')
+    print('| *     |')
+    print('|       |    ', b)
+    print('|     * |')
+    print(' -------')
+elif b == 3:
+    print(' _______')
+    print('| *     |')
+    print('|   *   |    ', b)
+    print('|     * |')
+    print(' -------')
+elif b == 4:
+    print(' _______')
+    print('| *   * |')
+    print('|       |    ', b)
+    print('| *   * |')
+    print(' -------')
+elif b == 5:
+    print(' _______')
+    print('| *   * |')
+    print('|   *   |    ', b)
+    print('| *   * |')
+    print(' -------')
+else:
+    print(' _______')
+    print('| *   * |')
+    print('| *   * |    ', b)
+    print('| *   * |')
+    print(' -------')
+if counter != 0:
+    print('ВСЕГО ХОДОВ ПРИ ВЫПОДЕНИИ КУША:', counter, 'раз')
+print()
+print()
